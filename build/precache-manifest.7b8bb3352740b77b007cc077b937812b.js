1ae96c60547334e03c1a265db33e4b8be5131d01
shwetbhungaliya78 <shwetpatel928@gmail.com>
2024-08-29 13:09:26 +0530

    my first

---
+++ build/precache-manifest.7b8bb3352740b77b007cc077b937812b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b62709fb7a46fba7090e1359a478a6",
+    "url": "/recharge-demo/index.html"
+  },
+  {
+    "revision": "9eac25f28a2ee0110808",
+    "url": "/recharge-demo/static/css/main.3274b984.chunk.css"
+  },
+  {
+    "revision": "395be05d07ec675b6b61",
+    "url": "/recharge-demo/static/js/2.afeb26e6.chunk.js"
+  },
+  {
+    "revision": "9eac25f28a2ee0110808",
+    "url": "/recharge-demo/static/js/main.823221c3.chunk.js"
+  },
+  {
+    "revision": "26e9d2d2e93e91f43939",
+    "url": "/recharge-demo/static/js/runtime~main.f5e23355.js"
+  }
+]);
